7cf21cf67b838615473af56997c5dca96b670c87
nguyenhachuy <nguyenhachuy@gmail.com>
2017-10-31 15:42:25 -0700

    First commit

---
+++ key.js
@@ -0,0 +1,11 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'tnSuMJVTBdBxyfQdI5qwp5PyE',
+  consumer_secret: 'TbSQxGzkq2zS6fCO5iYQF68qurbOHXqz20iruDeZy0az0fl1cw',
+  access_token_key: '925490117879271424-eIjxj291nw1ognXGr8leKVi1DOvXXkK',
+  access_token_secret: 'NE5Ho3v0Q3AogqXtD3Ob1b7WPwPNOo3wf0P5LEhjf0efP',
+}
+
+module.exports = twitterKeys;
+
---
+++ liri.js
@@ -0,0 +1,5 @@
+var twitter = require('twitterKeys');
+var consumer_key = twitter.consumer_key;
+var consumer_secret = twitter.consumer_secret;
+var access_token_key = twitter.access_token_key;
+var access_token_secret = twitter.access_token_secret;
